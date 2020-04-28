21e7d1ea549376c4bb4149288b879bf288dff339
Mert ÖZ <osemrt@gmail.com>
2020-04-28 06:24:52 +0300

    🔨 Rearrange table schemas

--- internal/Platform/Database/tables.sql
+++ internal/Platform/Database/tables.sql
@@ -1,157 +1,169 @@
 
-CREATE TABLE Person(
+CREATE TABLE PERSON(
 	id char(11) PRIMARY KEY,
-	fname varchar,
-	lname varchar,
+	fname varchar(30),
+	lname varchar(30),
 	phone_number text[],
 	home_number text[],
 	home_addr text,
 	work_addr text
 );	
-CREATE TABLE Login(
-	email char(64) NOT NULL,
+
+
+CREATE TABLE LOGIN(
+	email varchar(64) NOT NULL,
 	password char(64) NOT NULL,
-	person_id char(11) 								-- references to Turkish Identity number
-		--NOT NULL                                  --  
-		FOREIGN KEY REFERENCES Person(id)  							
-		ON DELETE SET NULL,
-	authorization_level int,						-- in order to create diff between instructor, registrar, and admin 
+	person_id char(11),			                       -- references to Turkish Identity number
+	--NOT NULL                                  	   -- optional, enable if every login is associated with a person 
+	authorization_level int,         		           -- in order to create diff between instructor, registrar, and admin
+
+	FOREIGN KEY (person_id) REFERENCES Person(id)  							
+		ON DELETE SET NULL	 
 );
 
 
 -- https://stackoverflow.com/questions/18656528/how-do-i-encrypt-passwords-with-postgresql
-CREATE TABLE Invoice(
+CREATE TABLE INVOICE(
 	id int PRIMARY KEY,							
 	invoice_date date NOT NULL,
 	total int NOT NULL,	
 	executo_id char(11) 								-- references to Turkish Identity number
-		NOT NULL 
-		FOREIGN KEY REFERENCES Person(id) 
-		DEFAULT '00000000000' 							
-		ON DELETE SET DEFAULT  							-- TRIGGER ON DELETE log_invoice_delete()
+		NOT NULL 		 
+		DEFAULT '00000000000',
+	
+	FOREIGN KEY (executo_id) REFERENCES Person(id)
+		ON DELETE SET DEFAULT  						    -- TRIGGER ON DELETE log_invoice_delete()
 );
-CREATE TABLE Payment(
+
+
+CREATE TABLE PAYMENT(
 	id int PRIMARY KEY,
 	pay_date date NOT NULL,
-	amount int NOT NULL,
+	amount varchar(10) NOT NULL,
 	invoice_id int 
 		NOT NULL 
-		FOREIGN KEY REFERENCES Invoice(id)
-		DEFAULT 0														
+		DEFAULT 0,
+	
+	FOREIGN KEY (invoice_id) REFERENCES Invoice(id)																
 		ON DELETE SET DEFAULT							-- TRIGGER ON DELETE log_invoice_delete()
 		ON UPDATE CASCADE
 );
 
 
-
-CREATE TABLE Branch(
-	name varchar(31) PRIMARY KEY,		-- Reasonable?
-	phone_number text[],			-- since multiple phone numbers may exists, array more reasonable?
+CREATE TABLE BRANCH(
+	name varchar(31) PRIMARY KEY,		
+	phone_number text[],			    
 	fax text[],
-	address NOT NULL text,
-	public_transport text[],			-- Public Transportation
+	address text NOT NULL,
+	public_transport text[],			
 	private_transport text[],			
 	facilities text[]
 );
 
-CREATE TABLE Course(
+
+CREATE TABLE COURSE(
 	id int PRIMARY KEY,
-	language NOT NULL varchar,
-	name varchar,
-	price int
+	language varchar(15) NOT NULL,
+	name varchar(15),
+	price varchar(10) NOT NULL
 );
 
-CREATE TABLE Sales(
-	invoice_number int 
-		FOREIGN KEY REFERENCES Invoice(id)
-		DEFAULT 0
-		ON DELETE SET DEFAULT
-		ON UPDATE CASCADE,						-- TRIGGER ON DELETE log_sales_delete()
-	customer_id char(11) 
-		FOREIGN KEY REFERENCES Person(id)
-		DEFAULT '00000000000'
-		ON DELETE SET DEFAULT
-		ON UPDATE CASCADE,						-- TRIGGER ON DELETE log_customer_delete() 
-	course_id int FOREIGN KEY REFERENCES Course(id),
+
+CREATE TABLE SALES(
+	invoice_number int		
+		DEFAULT 0,		
+	customer_id char(11)		
+		DEFAULT '00000000000',		
+	course_id int,
 	total int,
 	max_no_of_payments int,
-	PRIMARY KEY(invoice_number, customer_id)
+	
+	PRIMARY KEY(invoice_number, customer_id),
+	FOREIGN KEY (invoice_number) REFERENCES Invoice(id)
+		ON DELETE SET DEFAULT
+		ON UPDATE CASCADE,						-- TRIGGER ON DELETE log_sales_delete()
+	FOREIGN KEY (customer_id) REFERENCES Person(id)
+		ON DELETE SET DEFAULT
+		ON UPDATE CASCADE,						-- TRIGGER ON DELETE log_customer_delete() 
+	FOREIGN KEY (course_id) REFERENCES Course(id)
 );
 
-
-
-CREATE TABLE Lesson(
-	name text,
-	course_no int 
-		NOT NULL 
-		FOREIGN KEY REFERENCES Course(id)
-		DEFAULT 0
-		ON DELETE SET DEFAULT
-		ON UPDATE CASCADE,						-- TRIGGER ON DELETE log_lesson_course_delete() 
-	instructor_id char(11)
-		NOT NULL 
-		FOREIGN KEY REFERENCES Person(id)
-		DEFAULT '00000000000'
-		ON DELETE SET DEFAULT
-		ON UPDATE CASCADE,						-- TRIGGER ON DELETE log_lesson_instructor_delete() ,
-	classroom_id varchar 							-- no not null constraint, class may not have classroom, for ex online classes
-		FOREIGN KEY REFERENCES Classroom(id)
-		DEFAULT 0
-		ON DELETE SET NULL
-		ON UPDATE CASCADE,
-	lesson_date date,
-	lesson_ts timestamp,
-	PRIMARY KEY(name, course_no),
-);
-
-CREATE TABLE Classroom(
-	name varchar PRIMARY KEY, 
-	capacity NOT NULL int,
-	branch_name char(31) 
-		NOT NULL 
-		FOREIGN KEY REFERENCES Branch(name)
+CREATE TABLE CLASSROOM(
+	name varchar(20) PRIMARY KEY, 
+	capacity int NOT NULL,
+	branch_name varchar(31) 
+		NOT NULL,
+	
+	FOREIGN KEY (branch_name) REFERENCES Branch(name)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 );
 
-------------------- Zamanda Çalışabilir tablosu silindi, öğretmene 'possible_working_hours text' eklending
 
-CREATE TABLE Instructor(
-	id char(11) 
+CREATE TABLE LESSON(
+	name text,
+	course_no int 
+		NOT NULL 		
+		DEFAULT 0,	 
+	instructor_id char(11)
 		NOT NULL 
-		FOREIGN KEY REFERENCES Person(id) 
+		DEFAULT '00000000000',						
+	classroom_id varchar(20) 						 -- no not null constraint, class may not have classroom, for ex online classes
+		DEFAULT 0,
+	lesson_date varchar(20),
+	lesson_ts varchar(20),
+	
+	PRIMARY KEY(name, course_no),
+	FOREIGN KEY (course_no) REFERENCES Course(id)
+		ON DELETE SET DEFAULT
+		ON UPDATE CASCADE,   						 -- TRIGGER ON DELETE log_lesson_course_delete()
+	FOREIGN KEY (instructor_id) REFERENCES Person(id)		
+		ON DELETE SET DEFAULT
+		ON UPDATE CASCADE,							 -- TRIGGER ON DELETE log_lesson_instructor_delete()
+	FOREIGN KEY (classroom_id) REFERENCES Classroom(name)		
+		ON DELETE SET NULL
+		ON UPDATE CASCADE	
+);
+
+
+CREATE TABLE INSTRUCTOR(
+	id char(11),	
+	start_date date NOT NULL,
+	known_languages text[] NOT NULL,
+	pworking_hours char(21),		      -- possible working hours, 168bit map
+	
+	PRIMARY KEY(id),
+	FOREIGN KEY (id) REFERENCES Person(id) 
 		ON DELETE CASCADE 
-		ON UPDATE CASCADE,
-	start_date NOT NULL date,
-	known_languages NOT NULL text[],
-	pworking_hours char(168),		-- possible working hours, 168bit map
-	PRIMARY KEY(id)
+		ON UPDATE CASCADE
 );	
 
-CREATE TABLE InstructorCanWorkIn(
-	branch_name char(31) 
-		NOT NULL 
-		FOREIGN KEY REFERENCES Branch(name) 
-		ON DELETE CASCADE 
-		ON UPDATE CASCADE,
+
+CREATE TABLE INSTRUCTOR_CAN_WORK_IN(
+	branch_name varchar(31) 
+		NOT NULL,		
 	instructor_id char(11) 
-		NOT NULL 
-		FOREIGN KEY REFERENCES Instructor(id) 
-		ON DELETE CASCADE,
-	FOREIGN KEY(branch_name, Instructor_id)
-);
+		NOT NULL,
 
-CREATE TABLE works_in(
-	branch_name char(31) 
-		NOT NULL 
-		FOREIGN KEY REFERENCES Branch(id) 
+	PRIMARY KEY(branch_name, instructor_id),
+	FOREIGN KEY (branch_name) REFERENCES Branch(name) 
 		ON DELETE CASCADE 
 		ON UPDATE CASCADE,
-	person_id char(11) 
-		NOT NULL 
-		FOREIGN KEY REFERENCES Person(id) 
-		ON DELETE CASCADE,
-	PRIMARY KEY(branch_name, person_id)
+	FOREIGN KEY (instructor_id) REFERENCES Instructor(id) 
+		ON DELETE CASCADE
 );
 
 
+CREATE TABLE WORKS_ON(
+	branch_name varchar(31) 
+		NOT NULL,		
+	person_id char(11) 
+		NOT NULL,
+		
+	PRIMARY KEY(branch_name, person_id),
+	FOREIGN KEY (branch_name) REFERENCES Branch(name) 
+		ON DELETE CASCADE 
+		ON UPDATE CASCADE,
+	FOREIGN KEY (person_id) REFERENCES Person(id) 
+		ON DELETE CASCADE
+);
