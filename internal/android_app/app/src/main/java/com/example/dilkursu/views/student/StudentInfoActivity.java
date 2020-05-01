9645f90b097069a79a6cd1927d58fbbaac7f5529
Mert ÖZ <osemrt@gmail.com>
2020-05-01 08:34:38 +0300

    Connect StudentInfoActivity to database ~ Student

--- internal/android_app/app/src/main/java/com/example/dilkursu/views/student/StudentInfoActivity.java
+++ internal/android_app/app/src/main/java/com/example/dilkursu/views/student/StudentInfoActivity.java
@@ -1,16 +1,17 @@
 package com.example.dilkursu.views.student;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dilkursu.GlobalConfig;
 import com.example.dilkursu.R;
+import com.example.dilkursu.models.Student;
 
 public class StudentInfoActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageButton BtnBack;
@@ -25,6 +26,7 @@ public class StudentInfoActivity extends AppCompatActivity implements View.OnCli
     private EditText Branch;
     private EditText Payments;
     private EditText IdentityNo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,46 +34,61 @@ public class StudentInfoActivity extends AppCompatActivity implements View.OnCli
 
         defineVariables();
         defineListeners();
+        initViews();
     }
 
-    public void defineVariables(){
-        BtnBack = (ImageButton)findViewById( R.id.StudentInfoActivity_btn_back );
-        BtnEdit= (ImageButton)findViewById( R.id.StudentInfoActivity_btn_edit );
-        Name = (EditText) findViewById( R.id.StudentInfoActivity_edtTxt_name );
-        Surname = (EditText) findViewById( R.id.StudentInfoActivity_surname );
-        HomeTelephone = (EditText)findViewById( R.id.StudentInfoActivity_homeTelephone );
-        CellPhone = (EditText)findViewById( R.id.StudentInfoActivity_cellPhone );
-        Course = (EditText)findViewById( R.id.StudentInfoActivity_course );
-        Kur = (EditText)findViewById( R.id.StudentInfoActivity_kur );
-        Branch = (EditText)findViewById( R.id.StudentInfoActivity_branch );
-        Payments = (EditText)findViewById( R.id.StudentInfoActivity_payments );
-        IdentityNo = (EditText)findViewById( R.id.StudentInfoActivity_identityNo );
-        BtnSaveEdits = (Button)findViewById(R.id.StudentInfoActivity_btn_saveEdits);
+    public void defineVariables() {
+        BtnBack = (ImageButton) findViewById(R.id.StudentInfoActivity_btn_back);
+        BtnEdit = (ImageButton) findViewById(R.id.StudentInfoActivity_btn_edit);
+        Name = (EditText) findViewById(R.id.StudentInfoActivity_edtTxt_name);
+        Surname = (EditText) findViewById(R.id.StudentInfoActivity_surname);
+        HomeTelephone = (EditText) findViewById(R.id.StudentInfoActivity_homeTelephone);
+        CellPhone = (EditText) findViewById(R.id.StudentInfoActivity_cellPhone);
+        Course = (EditText) findViewById(R.id.StudentInfoActivity_course);
+        Kur = (EditText) findViewById(R.id.StudentInfoActivity_kur);
+        Branch = (EditText) findViewById(R.id.StudentInfoActivity_branch);
+        Payments = (EditText) findViewById(R.id.StudentInfoActivity_payments);
+        IdentityNo = (EditText) findViewById(R.id.StudentInfoActivity_identityNo);
+        BtnSaveEdits = (Button) findViewById(R.id.StudentInfoActivity_btn_saveEdits);
     }
 
-    public void defineListeners(){
-        BtnBack.setOnClickListener( this );
-        BtnEdit.setOnClickListener( this );
+    public void defineListeners() {
+        BtnBack.setOnClickListener(this);
+        BtnEdit.setOnClickListener(this);
         BtnSaveEdits.setOnClickListener(this);
     }
+
+    private void initViews() {
+        Name.setText(GlobalConfig.currentUser.getFname());
+        Surname.setText(GlobalConfig.currentUser.getLname());
+        HomeTelephone.setText(GlobalConfig.currentUser.getHomeNumbers().get(0));
+        CellPhone.setText(GlobalConfig.currentUser.getPhoneNumbers().get(0));
+        Course.setText(Integer.toString(((Student) GlobalConfig.currentUser).getGroupNo()));
+        Branch.setText(((Student) GlobalConfig.currentUser).getBranchName());
+        IdentityNo.setText(((Student) GlobalConfig.currentUser).getId());
+        Kur.setText(((Student) GlobalConfig.currentUser).getCourse().getName());
+
+
+    }
+
     @Override
     public void onClick(View v) {
-        if ( v == BtnBack ) {
+        if (v == BtnBack) {
             // Handle clicks for BtnBack
             finish();
-        } else if ( v == BtnEdit ) {
+        } else if (v == BtnEdit) {
             // Handle clicks for BtnEdits
             setEditable(true);
-        }else if ( v == BtnSaveEdits ) {
+        } else if (v == BtnSaveEdits) {
             setEditable(false);
-            if( updateStudentInfo() ) //TODO save changes of student info to database
+            if (updateStudentInfo()) //TODO save changes of student info to database
                 Toast.makeText(this, "Bilgileriniz güncellendi", Toast.LENGTH_SHORT).show();
             else
                 Toast.makeText(this, "İşlem Başarısız", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void setEditable(boolean mode){
+    private void setEditable(boolean mode) {
         Name.setClickable(mode);
         Name.setFocusable(mode);
         Name.setFocusableInTouchMode(mode);
@@ -108,11 +125,10 @@ public class StudentInfoActivity extends AppCompatActivity implements View.OnCli
         IdentityNo.setFocusable(mode);
         IdentityNo.setFocusableInTouchMode(mode);
 
-        if(mode) {
+        if (mode) {
             BtnSaveEdits.setVisibility(View.VISIBLE);
             BtnEdit.setBackgroundResource(R.drawable.edit2);
-        }
-        else {
+        } else {
             BtnSaveEdits.setVisibility(View.INVISIBLE);
             BtnEdit.setBackgroundResource(R.drawable.edit);
         }
@@ -120,7 +136,7 @@ public class StudentInfoActivity extends AppCompatActivity implements View.OnCli
 
     }
 
-    private boolean updateStudentInfo(){
+    private boolean updateStudentInfo() {
 
         Name.getText().toString();
         Surname.getText().toString();
