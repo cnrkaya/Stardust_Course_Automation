0721c6dd2d49753590f4f774a574a08e5bd81daa
Mert ÖZ <osemrt@gmail.com>
2020-05-02 17:49:48 +0300

    Fix parsing bug

--- internal/android_app/app/src/main/java/com/example/dilkursu/repository/TextProcessor.java
+++ internal/android_app/app/src/main/java/com/example/dilkursu/repository/TextProcessor.java
@@ -5,17 +5,42 @@ import java.util.ArrayList;
 public class TextProcessor {
 
     public static ArrayList<String> stringToArray(String string) {
+        //TODO: Need refactor after database data cleaning
         ArrayList<String> items = new ArrayList<>();
-        string = string.substring(string.indexOf("{") + 1);
-        string = string.substring(0, string.indexOf("}"));
+        if (string.contains("{[")) {
+            string = string.substring(string.indexOf("{") + 1);
+            string = string.substring(0, string.indexOf("}"));
 
-        String[] temp = string.split(",");
-        for (String s : temp) {
-            s = s.substring(s.indexOf('"') + 1);
-            s = s.substring(0, s.indexOf('"'));
-            items.add(s);
+            string = string.substring(string.indexOf("[") + 1);
+            string = string.substring(0, string.indexOf("]"));
+
+            String[] temp = string.split(",");
+            for (String s : temp) {
+//                s = s.substring(s.indexOf('"') + 1);
+//                s = s.substring(0, s.indexOf('"'));
+                items.add(s);
+            }
+        } else {
+            string = string.substring(string.indexOf("{") + 1);
+            string = string.substring(0, string.indexOf("}"));
+
+            String[] temp = string.split(",");
+            for (String s : temp) {
+                s = s.substring(s.indexOf('"') + 1);
+                s = s.substring(0, s.indexOf('"'));
+                items.add(s);
+            }
+
         }
 
+        return items;
+    }
+
+    public static String[] parseRecords(String string) {
+        string = string.substring(string.indexOf("(") + 1);
+        string = string.substring(0, string.indexOf(")"));
+
+        String[] items = string.split(",");
         return items;
     }
 
